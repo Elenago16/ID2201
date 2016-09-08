826cb070dea1ea0822354e7968bb0cfaef82b6a0
Elena Goncharuk <gonelvik@hotmail.com>
2016-09-08 22:25:28 +0200

    Create pingpong.erl

---
+++ Intro/pingpong.erl
@@ -0,0 +1,23 @@
+-module(pingpong).
+
+-export([start_ping/1, start_pong/0, ping/1, pong/0]).
+
+ping(Pong_Node) ->
+	{pong, Pong_Node} ! self(),
+
+receive
+      "hello there" -> 
+		  io:format("I got the message!~n")
+end.
+
+pong() ->
+    receive
+      X -> 
+		  X ! "hello there"
+end.
+
+start_pong() ->
+    register(pong, spawn(pingpong, pong, [])).
+
+start_ping(Pong_Node) ->
+    spawn(pingpong, ping, [Pong_Node]).
